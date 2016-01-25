9a1bd3ade0d542ddaf89776b27aec1e33e387bb0
XiaoruiZhu <zhuxiaorui1989@gmail.com>
2016-01-25 16:28:57 -0500

    Still changing!

--- AdpQMLE/R/MyMLE.R
+++ AdpQMLE/R/MyMLE.R
@@ -1,18 +1,27 @@
-##正态GARCH(1,1)的mle
-MyMLE=function(h,x){
-  ##基于普通的normal distribution innovation likelihood
-  lik<-function(x){
-    TTL=function(par){
-      n=length(x)
-      sig2=numeric(n);tem1=numeric(1);tem2=numeric(1)
-      w=par[1]
-      alpha=par[2]
-      beta=par[3]
-      sig2[1]=w/(1-alpha-beta)
+#' Simple MLE based on normal residua assumption.
+#' 
+#'
+#' @param x is time series data. 
+#'
+#' @return is the estimated parameters of GARCH(1,1)
+#' @export
+#'
+#' @examples
+MyMLE <- function(x){
+  ## normal distribution innovation likelihood
+  lik <- function(x){
+    TTL <- function(par){
+      n <- length(x)
+      sig2 <- numeric(n); tem1 <- numeric(1); tem2 <- numeric(1)
+      w <- par[1]
+      alpha <- par[2]
+      beta <- par[3]
+      sig2[1] <- w/(1-alpha-beta)
       for (t in 2:n){
         tem2=beta*(tem2+tem1)
         tem1=x[t-1]^2
-        sig2[t]=(w/(1-beta)+alpha*(tem1+tem2)) #这按照笔记红色的通项公式改写可计算GARCH(P,Q)  
+        sig2[t]=(w/(1-beta)+alpha*(tem1+tem2)) 
+        #Change this part based on my NOTES in red GARCH(P,Q)  
       }
       if (c(w,alpha,beta) > rep(0,3) && (alpha+beta) < 1 && sig2>=0)  
       {
