eec61372db148eb9965454e0f587552a738dbf69
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-03-20 13:04:03 -0400

    2016/03/20

--- main.R
+++ main.R
@@ -13,21 +13,10 @@ kurtosis(xx)
 
 ########### 2. The basic MLE method testing. ###############
 # Test for MLE function #
-n <- 1100
-a <- c(0.1, 0.5)
-b <- c(0.2) # GARCH(1,1) coefficients
-e <- rnorm(n)
-x <- double(n)
-sig2t <- x
-x[1:1] <- rnorm(1, sd = sqrt(a[1]/(1.0-a[2]-b[1])))
-x[1]
-sig2t[1] <- a[1]/(1.0-a[2]-b[1])
-sig2t[1]
-for(i in 2:n) {
-  # Generate GARCH(1,1) process
-  sig2t[i] = sum(a * c(1, x[i-1]^2)) + sum(b[1] * sig2t[i - 1])
-  x[i] = e[i]*sqrt(sig2t[i])
-}
+alpha <- c(0.1, 0.5)
+beta <- c(0.2) # GARCH(1,1) coefficients
+x.e.sig <- GARCH1_1(n=1000, a = alpha, b = beta)
+x <- x.e.sig$series 
 
 # x <- ts(x[101:1100])
 
@@ -36,16 +25,25 @@ library(tseries)
 x.arch <- garch(x, order = c(1,1)) # Fit GARCH(1,1)
 est1 <- MLE(series = x, LogLFunc = LogL_GARCH_Norm)
 est1
-est2 <- tQMLE(series = x, logLFunc = LogL_GARCH_t)
+est2 <- tQMLE(series = x, LogLFunc = LogL_GARCH_t, dfest = 20)
+est2
+
+# Finished test for MLE and GARCH with normal innovation.
 
 
-xx.arch <- garch(xx, order = c(1,1)) # Fit GARCH(1,1)
-para2 <- MyMLE(xx)
-para2
-para3 <- MytQMLE(xx, 2.3)
-para3
+###########################################################################
+# Test tQMLE for GARCH with t
+xx <- GARCH_t(alpha = c(0.1, 0.2), beta = 0.5, n = 200, rnd = "rt", df.t = 2.3)
+plot(xx, type = "l")
+# library(timeDate)
+kurtosis(xx)
+estxxMLE <- MLE(series = xx, LogLFunc = LogL_GARCH_Norm)
+estxxMLE
+estxx <- tQMLE(series = xx, LogLFunc = LogL_GARCH_t, dfest = 2.1)
+estxx
+# Done, with known degree of freedom of t innovation, the tQMLE is better than MLE with normal
+###########################################################################
 
-############# change myMLE for GARCH(p,q) ###############
 
 
 ############# 3. com.residue function writing and testing ###################
