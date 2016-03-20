663e41704620af28ac50b3a901e891f92d41fc53
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-03-19 21:30:11 -0400

    Test example for optimization of quadratic funciton.

---
+++ AdpQMLE/R/optimx_example.R
@@ -0,0 +1,23 @@
+costFunc = function (x)
+{
+  return(sum((x-1.3)^4))
+}
+
+grad = function(x) {
+  return(4*(x-1.3)^3)
+}
+
+library(optimx)
+startTime = proc.time()
+results = optimx(seq(1, 50,.1), fn=costFunc, gr = grad, itnmax=50, method=c("Rcgmin"))
+proc.time() - startTime
+
+
+x <- seq(1, 50, .1)
+
+startTime = proc.time()
+results2 <- nlminb(start = c(0), objective = costFunc, lower = -Inf, upper = Inf)
+proc.time() - startTime
+
+results
+results2
