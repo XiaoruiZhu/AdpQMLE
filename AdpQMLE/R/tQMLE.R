9473a73c4b2c39171a2a0453ba59a512300c7616
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-03-20 13:05:25 -0400

    Finished tQMLE method.

--- AdpQMLE/R/tQMLE.R
+++ AdpQMLE/R/tQMLE.R
@@ -1,7 +1,8 @@
-#' MytQMLE is a function used to estimate parameters of GARCH(1,1) with student's t innovation.
+#' tQMLE is a function used to estimate parameters of GARCH(1,1) with student's t innovation.
 #'
-#' @param x is the time series that was hypothesized as GARCH(1,1) model. 
 #' @param dfest is specified degree of freedom of innovation. 
+#' @param series The orginal time series, which need to be fitted as GARCH(1,1)  
+#' @param LogLFunc is the log likelihood function of GARCH(1,1) with student's t innovation. This input can be changed according to  
 #'
 #' @return
 #' @export
@@ -13,7 +14,7 @@ tQMLE=function(series, LogLFunc, dfest){
   df=dfest
   
   #plot(c(1:500),x,type='l')
-  tQMLE.N=nlminb(c(0.01,0.01,0.01),LogLFunc(seires),lower=c(0,0,0),upper=c(Inf,1,1))
+  tQMLE.N=nlminb(c(0.01,0.01,0.01),LogLFunc(series,df),lower=c(0,0,0),upper=c(Inf,1,1))
   #mle.N
   list(tQMLE.N=tQMLE.N$par)
 }
