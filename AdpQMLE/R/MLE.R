02d50545cd28f3170944438967cb3acaa1fcd8a1
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-03-19 23:20:10 -0400

    Finished Maximum Likelihood Estimation!

---
+++ AdpQMLE/R/MLE.R
@@ -0,0 +1,16 @@
+#' Simple Maximum Likelihood Estimation based on normal residual assumption.
+#' 
+#'
+#' @param series is time series data. It is need to be revise.
+#' @param LogLFunc 
+#'
+#' @return is the estimated parameters of GARCH(1,1)
+#' @export
+#'
+#' @examples
+MLE <- function(series, LogLFunc){
+  ## normal distribution innovation likelihood
+  # series <- series
+  MLE.N <- nlminb(c(0.01,0.01,0.01), LogLFunc(series), lower=c(0,0,0), upper=c(Inf,1,1))
+  list(MLE.N = MLE.N$par)
+}
