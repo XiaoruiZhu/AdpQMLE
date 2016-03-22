4ce5d69139fe1905a36b229f79f0266323e5c78b
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-03-22 15:15:01 -0400

    2016/03/22

--- main.R
+++ main.R
@@ -33,25 +33,66 @@ est2
 
 ###########################################################################
 # Test tQMLE for GARCH with t
-xx <- GARCH_t(alpha = c(0.1, 0.2), beta = 0.5, n = 200, rnd = "rt", df.t = 2.3)
+xx <- GARCH_t(alpha = c(0.1, 0.2), beta = 0.5, n = 1000, rnd = "rt", df.t = 2.3)
 plot(xx, type = "l")
-# library(timeDate)
+library(timeDate)
 kurtosis(xx)
-estxxMLE <- MLE(series = xx, LogLFunc = LogL_GARCH_Norm)
+estxxMLE <- MLE(y = xx, LogLFunc = "LogL_GARCH_Norm")
+x.arch <- garch(xx, order = c(1,1)) # Fit GARCH(1,1)
 estxxMLE
-estxx <- tQMLE(series = xx, LogLFunc = LogL_GARCH_t, dfest = 2.1)
+
+estxx <- tQMLE(series = xx, LogLFunc = "LogL_GARCH_t", dfest = 5)
 estxx
+
 # Done, with known degree of freedom of t innovation, the tQMLE is better than MLE with normal
 ###########################################################################
 
+# 3. Creating MLE for linear regression and test #####
 
+# Fix x values for all runs of the simulation; draw from an exponential
+n <- 2000 # So we don't have magic #s floating around
+beta.0 <- 5
+beta.1 <- -2
+sigma.sq <- 1
+fixed.x <- rexp(n=n)
+# Simulate from the model Y=\beta_0+\beta_1*x+N(0,\sigma^2)
+# Inputs: intercept; slope; variance; vector of x; return sample or estimated
+# linear model?
+# Outputs: data frame with columns x and y OR linear model fit to simulated y
+# regressed on x
+data <- gen.lin(intercept=beta.0, slope = beta.1, noise.variance=sigma.sq, x=fixed.x, dis.error = "rt", dft=3)
+head(data)
+par(mfrow=c(1,1))
+hist(fixed.x, freq = FALSE, breaks = 50, xlab = expression(x[i]), main = "")
+hist(data$y, freq = FALSE, breaks = 50, xlab = expression(y[i]), main = "")
+plot(data$x, data$y)
+test <- lm(data$y~data$x)
+test
+y <- as.matrix(data$y)
+y
+X <- as.matrix(data$x)
+X
+myMLE <- MLE(y, X, LogLFunc = "LogL_Linear_Norm")
+myMLE
+
+# 
+# par(mfrow=c(2,1))
+# slope.sample <- replicate(1e4, coefficients(sim.lin.gauss(model=TRUE))["x"])
+# hist(slope.sample,freq=FALSE,breaks=50,xlab=expression(hat(beta)[1]),main="")
+# curve(dnorm(x,-2,sd=sqrt(3/(n*var(fixed.x)))), add=TRUE, col="blue")
+# pred.sample <- replicate(1e4, predict(sim.lin.gauss(model=TRUE),
+#                                       newdata=data.frame(x=-1)))
+# hist(pred.sample, freq=FALSE, breaks=50, xlab=expression(hat(m)(-1)),main="")
+# curve(dnorm(x, mean=beta.0+beta.1*(-1),
+#             sd=sqrt((sigma.sq/n)*(1+(-1-mean(fixed.x))^2/var(fixed.x)))),
+#       add=TRUE,col="blue")
+
+#####################################################
 
 ############# 3. com.residue function writing and testing ###################
 
 ################################
 
-############# 4. MytQMLE function writing and testing ###################
-################################
 
 ############# 5. EstmBestdf function writing and testing ###################
 ################################
