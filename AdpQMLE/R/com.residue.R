bbb3d504be42267b550408efd0b48e00a53bb050
XiaoruiZhu <zhuxiaorui1989@gmail.com>
2016-03-28 15:35:25 -0400

    Finished function for residues calculation.

--- AdpQMLE/R/com.residue.R
+++ AdpQMLE/R/com.residue.R
@@ -1,28 +1,24 @@
 #' com.residue is a function used to calculate the residue of GARCH model so that the futher adaptive procedure can be move forward.  
 #'
-#' @param para is the parameters of GARCH model. Here only consider the GARCH(1,1), so there are three parameters. But this whole stuff should be revised for general models.
-#' @param x is the time series. It could be daily stock returns, minutely stock returns, or monthly exchange rate.  
+#' @param alpha Includes intercept and all parameters of ARCH terms. 
+#' @param beta Includes all parameters of GARCH terms. 
+#' @param series Time series.
 #'
-#' @return  is the residues between the predicted time series and the original time series. 
+#' @return  Return a list contains estimators, residues and conditional sigma square. 
 #'
 #' @examples
-com.residue <- function(para,x){
-  n <- length(x)
-  sig2 <- numeric(n); tem1 <- numeric(1); tem2 <- numeric(1)
-  w <- para[1]
-  alpha <- para[2]
-  beta <- para[3]
-  if (w==0) sig2[1] <- abs(x[1])
-  else sig2[1] <- w/(1-alpha-beta)
-  for (t in 2:n){
-    # tem1=tem1+beta^(t-2)
-    # tem2=beta*(tem2)+x[t-1]^2
-    # sig[t]=w/(1-beta)+alpha*(tem2)+beta^(t-1)*sig[1]
-    tem2 <- beta*(tem2+tem1)
-    tem1=x[t-1]^2
-    sig2[t]=(w/(1-beta)+alpha*(tem1+tem2)) 
-    #This part can be changed for GARCH(P,Q)
+com.residue <- function(alpha, beta, series){
+  n <- length(series)
+  sig2 <- numeric(n)
+  q <- length(alpha)-1
+  p <- length(beta)
+  d <- max(q,p)
+  if (alpha[1]==0) sig2[1:d] <- abs(series[1:d])
+  else sig2[1:d] <- alpha[1]/(1.0-sum(alpha[2:(q+1)])-sum(beta))
+  for (t in (d+1):n){
+    sig2[t] <-  sum(alpha * c(1, series[t - (1:q)]^2)) + sum(beta * sig2[t - (1:p)])
+    # It is changed based on my NOTES in red GARCH(P,Q)  
   }
-  e=x/(sqrt(sig2))
-  return(e)
+  e <- series/(sqrt(sig2))
+  list(sig.sq = sig2, e=e)
 }
