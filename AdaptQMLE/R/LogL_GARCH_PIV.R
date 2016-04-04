38ff3b9aeb6751eb4afc8da82842abd8b5448e32
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-04-03 22:03:14 -0400

    Log-likelihood function of GARCH with Pearson IV innovation.

--- AdaptQMLE/R/LogL_GARCH_PIV.R
+++ AdaptQMLE/R/LogL_GARCH_PIV.R
@@ -1,15 +1,25 @@
 #' The Log-likelihood function of GARCH model with Pearson's Type IV (PIV) distributed error
 #'
-#' @title log-likelihood function of GARCH(p,q) with Pearson's Type IV (PIV) distribution
+#' @title log-likelihood function of GARCH(p,q) with PIV distribution
 #'
 #' @param series The time series that need to be fitted.
 #' @param p The order of GARCH terms \eqn{\sigma^2}
 #' @param q The order of ARCH terms \eqn{\epsilon^2}
-#' @param df The specified degree of freedome of student's t innovation.
+#' @param lambda \eqn{\lambda} is the location parameter of Pearson's Type IV distribution 
+#' @param a \eqn{a} is the scale parameters of PIV distribution with condition \eqn{a > 0}
+#' @param nu \eqn{\nu} parameter of PIV distribution. \eqn{\nu} is related to the
+#'        asymmetry of the distribution, and a positive (or negative) \eqn{\nu} stands 
+#'        for a negatively (or positively) skewed distribution
+#' 
+#' @param m The \eqn{m} parameter of PIV distribution with condition \eqn{m > 1/2}. m 
+#'        captures the leptokurtosis of the distribution, and a smaller value of m represents 
+#'        a heavier tail of the distribution
 #'
+#' @importFrom pracma gammaz
+#' 
 #' @return Log-Likelihood function of GARCH with t error
 #'
-LogL_GARCH_PIV <- function(series, p, q, df){
+LogL_GARCH_PIV <- function(series, p, q, lambda, a, nu, m){
   GARCH_e_PIV <- function(para){
     n <- length(series)
     sig2 <- numeric(n)
@@ -22,9 +32,10 @@ LogL_GARCH_PIV <- function(series, p, q, df){
     }
     if (c(alpha,beta) > rep(0,(q+p+1)) && (sum(alpha[2:(q+1)])+sum(beta)) < 1 && sig2>=0)
     {
-      tQMLE.e <- series / (sqrt(sig2))
-      g <- (gamma((df + 1)/2) / (pi * df)^0.5 / gamma(df/2))*(1+tQMLE.e^2/(df))^(-(df+1)/2)
-      return(sum(log(sqrt(sig2))-log(g)))
+      e <- series / (sqrt(sig2))
+      K <- 2^(2*m-2)*abs(gammaz(m+nui/2))^2/(a*pi*gammaz(2*m-1))
+      f <- K * (1 + ((e - lambda)/a)^2)^(-m) * exp(-nu * tan((e - lambda)/a)^(-1))
+      return(sum(log(sqrt(sig2))-log(f)))
     }
     else return(Inf)
   }
