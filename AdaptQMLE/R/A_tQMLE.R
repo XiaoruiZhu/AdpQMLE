affdcf07dc03892295b35394bdb7d156e85fc857
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-04-03 22:00:48 -0400

    Changed for iteration stop.

--- AdaptQMLE/R/A_tQMLE.R
+++ AdaptQMLE/R/A_tQMLE.R
@@ -20,17 +20,18 @@ A_tQMLE <- function(series, order = c(1,1)){
   q <- order[1]; p <- order[2]
   n <- length(series); max.iter <- 50
   Bdf.t <- c()
-  # Estm contains parameters, est.df, YITA and SSE(total sum square error)
+  # Estm contains parameters, est.df, ETA and SSE(total sum square error)
   Estm <- matrix(NA, max.iter, (p+q+1+3))
   Est.model <- tQMLE(series=series, LogLFunc = "LogL_GARCH_Norm", order = order)
   para <- Est.model$QMLE.N
   e.t <- Est.model$e
   Estm <- c(Est.model$QMLE.N, NA, NA, NA)
+  names(Estm) <- c("alpha0", "alpha1", "beta","est.df", "eta", "diff_Para")
   # The scale parameter eta_f is the boundary. Equal to 1 is the condition.
   new.df <- Estdf(e.t)
   new.YITA <- YITAtQMLE(e=e.t,dfest=new.df)
-  iter = 1
-  while (iter<max.iter & diff > 0.00001) {
+  iter <-  1; diff <- 1
+  while (iter<max.iter & diff > 0.0000001) {
     iter <-  iter+1
     Est.model <- tQMLE(series = series, LogLFunc = "LogL_GARCH_t", order = order, dfest = new.df)
     e.t <- Est.model$e
@@ -39,7 +40,7 @@ A_tQMLE <- function(series, order = c(1,1)){
     Estm <- rbind(Estm, c(Est.model$QMLE.t, new.df, new.YITA, new.diff))
     new.df <- Estdf(e.t)
     new.YITA <- YITAtQMLE(e=e.t,dfest=new.df) # This is the crux! Using the assumed df to estimate yita!
-    diff <- new.diff
+    diff <- new.diff; para <- new.para
   }
   return(Estm)
 }
